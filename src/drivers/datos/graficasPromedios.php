9a44ceb56af09c68f8d008e4f8e5c9e57d271ae0
berserkerChrist <alberto198tr@gmail.com>
2019-02-07 08:02:07 -0600

    version 2.2.7

--- src/drivers/datos/graficasPromedios.php
+++ src/drivers/datos/graficasPromedios.php
@@ -13,12 +13,9 @@
     $periodo = $con->real_escape_string($_POST['periodoGraficasPromedios']);
     $alumno = $_SESSION['hijo'];
 
-    //echo "$grupo $periodo $materia $alumno";
-
     $sql = "SELECT * FROM promedios INNER JOIN materia ON promedios.materia = materia.id
     WHERE promedios.id_alumno = '".$alumno."' AND promedios.periodo = '".$periodo."' AND promedios.grupo = '".$grupo."'";
     $resultado = mysqli_query($con, $sql);
-    //$fila = mysqli_fetch_array($resultado);
     $data = array();
     foreach ($resultado as $fila) {
       $data[] = $fila;
